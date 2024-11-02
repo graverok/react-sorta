5ab5213ee4c63cfd266fa7928e70845cf1e19f5b
Aleksandr Gurianov <hello@golova.pro>
2024-11-02 14:04:26 +0500

    Remove dev files

--- dev/lists/index.ts
+++
@@ -1,5 +0,0 @@
-export { SimpleList } from "./simple";
-export { HandleList } from "./handle";
-export { NumberList } from "./number";
-export { ScrollList } from "./scroll";
-export { VirtualList } from "./virtual";
